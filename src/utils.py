9edf1fa2d4c61127a4fed8c9226e6929ff6c3197
Rocky <rockyjoseph055@gmail.com>
2023-09-19 16:51:18 +0530

    done

--- src/utils.py
+++ src/utils.py
@@ -29,18 +29,14 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params):
             model = list(models.values())[i]
             param = params[list(models.keys())[i]]
 
-            # model.fit(X_train, y_train)
-
             rs_grid = RandomizedSearchCV(model, param, cv=5)
             rs_grid.fit(X_train,y_train)
 
             model.set_params(**rs_grid.best_params_)
             model.fit(X_train, y_train)
 
-            # y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
-            # train_model_score = accuracy_score(y_train, y_train_pred)
             test_model_score = classification_report(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
