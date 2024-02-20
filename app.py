76b0d08ae5f8a50333ff067e87a438b814d4fa12
Rocky Joseph <131973908+rockyjoseph@users.noreply.github.com>
2024-02-20 17:14:17 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 # DATASET
-df = pd.read_csv('notebook\data\Crop_recommendation.csv')
+df = pd.read_csv('notebook\data\Crop_recommendation.csv.pathlib')
 
 # USER MENU
 st.sidebar.title('CROPS RECOMMENDER APP')
