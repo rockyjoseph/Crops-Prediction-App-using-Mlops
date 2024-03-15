1442433f4e03851c063fef967350ea770c14215b
Rocky Joseph <131973908+rockyjoseph@users.noreply.github.com>
2024-03-15 20:23:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 # DATASET
-df = pd.read_csv('notebook/data/Crop_recommendation.csv')
+df = pd.read_csv('artifacts/data.csv')
 
 # USER MENU
 st.sidebar.title('CROPS RECOMMENDER APP')
 user_menu = st.sidebar.radio('Select an Option',
-                             ('Dataset', 'EDA', 'Requirements', 'App'))
+                             ('Dataset', 'Requirements', 'App'))
 
 
 if user_menu == 'Dataset':
@@ -30,115 +30,6 @@ if user_menu == 'Dataset':
     # DESCRIPTION OF THE DATA
     st.header('Description of the data')
     st.table(df.describe())
-
-if user_menu == 'EDA':
-    # TITLE 
-    st.title('Explorartory Data Analysis (EDA)')
-
-    # VARIABLES DECLARATION
-    nitrogen = df['nitrogen'].nunique()
-    phosphorus = df['phosphorus'].nunique()
-    potassium = df['potassium'].nunique()
-    temperature = df['temperature'].nunique()
-    humidity = df['humidity'].nunique()
-    ph = df['ph'].nunique()
-    rainfall = df['rainfall'].nunique()
-    label = df['label'].nunique()
-
-    st.header('Question 1')
-    st.write('What are the unique values in each features in the data?')
-
-    # TOP STATS
-    st.title("Top Statistics")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.header("Nitrogen")
-        st.title(nitrogen)
-    with col2:
-        st.header("Phosphorus")
-        st.title(phosphorus)
-    with col3:
-        st.header("Potassium")
-        st.title(potassium)
-
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.header("Temperature")
-        st.title(temperature)
-    with col2:
-        st.header("Humidity")
-        st.title(humidity)
-    with col3:
-        st.header("ph")
-        st.title(ph)
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.header("Rainfall")
-        st.title(rainfall)
-    with col2:
-        st.header("Label")
-        st.title(label)
-
-    # COUNTPLOT VISUALISATION
-    st.header('Question 2')
-    st.write('What are the count values in each features in the data?')
-
-    countplot_name = st.selectbox('Features for Value Counts & Outlier Detection', df.columns)
-
-    fig = px.histogram(df, x=countplot_name, title=f'{countplot_name} counplot visualisation')
-    st.plotly_chart(fig)
-
-    fig = px.bar(df, y=countplot_name, color='label', title=f'{countplot_name} with stroke')
-    st.plotly_chart(fig)
-
-    # OUTLIER DETECTION
-    st.header('Question 3')
-    st.write('What if there are any outliers in the data?')
-
-    fig = px.box(df, y=countplot_name, color='label', title='Outlier Detection (Boxplot)')
-    st.plotly_chart(fig)
-    countplot_name
-    fig = px.violin(df, y=countplot_name, color='label', title='Outlier Detection (Violinplot)')
-    st.plotly_chart(fig)
-
-    # SORTING & CHECKING BY MEAN VALUES
-    st.header('Question 3')
-    st.write('Which crops requires more or less features? Why not check it by the mean?')
-
-    sorting = df.groupby(by='label').mean().reset_index()
-    st.table(sorting)
-
-    # CONCLUSION FROM MEAN VALUES
-    st.header('From above Observations the mean values concludes:')
-
-    st.markdown('''
-        - Cotton requires most Nitrogen.
-        - Apple requires most Phosphorus.
-        - Grapes requires most Potassium.
-        - Papaya requires a hot climate.
-        - Coconut requires a humid climate.
-        - Chickpea requires high ph in soil.
-        - Rice requires huge amount of rainfall or water.
-    ''')
-
-    st.header('Question 4')
-    st.write("Can we use PCA for multivariate labels in the data?")
-
-    # PCA FOR 2-DIMENSIONAL PLOT
-    pca = PCA(n_components=2)
-    df_pca = pca.fit_transform(df.drop(['label'], axis=1))
-    df_pca = pd.DataFrame(df_pca)
-    fig = px.scatter(x=df_pca[0], y=df_pca[1], color=df['label'], title="2-Dimensional plot")
-    st.plotly_chart(fig)
-
-    # PCA FOR 3-DIMENSIONAL PLOT
-    pca_3d = PCA(n_components=3)
-    df_pca3d = pca_3d.fit_transform(df.drop(['label'], axis=1))
-    df_pca3d = pd.DataFrame(df_pca3d)
-    fig = px.scatter_3d(x = df_pca3d[0], y = df_pca3d[1], z = df_pca3d[2], color = df['label'],
-                        title="3-Dimensional plot")
-    st.plotly_chart(fig)
 
 if user_menu == 'Requirements':
     # TITLE
